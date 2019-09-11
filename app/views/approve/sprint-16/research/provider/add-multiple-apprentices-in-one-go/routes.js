ea669efb05da2dc853f54fa1a210aa83f791c449
rob.langdon@digital.education.gov.uk <rob.langdon@digital.education.gov.uk>
2019-09-11 11:34:40 +0100

    Sprint 16: Upload journey

--- app/views/approve/sprint-16/research/provider/add-multiple-apprentices-in-one-go/routes.js
+++ app/views/approve/sprint-16/research/provider/add-multiple-apprentices-in-one-go/routes.js
@@ -9,44 +9,17 @@ router.get('/', (req, res) => {
 	res.redirect(`/${req.feature}/email`)
 })
 
-//review an apprentice
-router.post('/start-adding', (req, res) => {
-	if (req.session.data['approve'] == 'add-1') {
-	  res.redirect('apprentice-details')
-	} else if (req.session.data['approve'] == 'upload') {
-		res.redirect('upload')
-	  }
-	  else if (req.session.data['approve'] == 'approve') {
-		res.redirect('approval-confirmed')
-	  } else {
-	  res.redirect('#')
-	}
-})
-
-router.post('/review-cohort', (req, res) => {
-	if (req.session.data['approve'] == 'add-1') {
-	  res.redirect('apprentice-details')
-	} else if (req.session.data['approve'] == 'upload') {
-		res.redirect('upload')
-	  }
-	  else if (req.session.data['approve'] == 'approve') {
-		res.redirect('approval-confirmed')
-	  } else {
-	  res.redirect('#')
-	}
-})
-
 // Confirm employer (add)
 router.post('/add__confirm-employer', (req, res) => {
 	if (req.session.data['confirm-employer'] == 'yes' ) {
-		res.redirect(`start-adding-provider`)
+		res.redirect(`start-adding`)
 	} else {
 		req.session.data['employer'] = ''
 		res.redirect(`add__choose-employer`)
 	}
 })
   
-//end of journey screens
+// End of journey screens
 router.post('/confirmation', (req, res) => {
 	if (req.session.data['next-step'] == 'upload') {
 	  res.redirect('add__choose-employer')
